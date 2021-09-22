9e3f265af838e196ce16ada969ea36c90f07e91b
huanglinxian <linxian963@gmail.com>
2021-09-21 19:40:52 -0500

    init

---
+++ pyqt5webpage/child_GUI/Qtapp/QVAR.cpp
@@ -0,0 +1,29 @@
+#include "QVAR.h"
+#include "ui_QVAR.h"
+
+QVAR::QVAR(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::QVAR)
+{
+    ui->setupUi(this);
+}
+
+QVAR::~QVAR()
+{
+    delete ui;
+}
+
+void QVAR::on_pushButton_clicked()
+{
+
+}
+
+void QVAR::on_pushButton_2_clicked()
+{
+
+}
+
+void QVAR::on_pushButton_3_clicked()
+{
+
+}
